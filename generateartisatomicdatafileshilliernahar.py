945ec3ccd4dd0c48c43ed78ce1c63e4d0871ed0e
Luke Shingles <luke.shingles@gmail.com>
2016-03-11 00:46:08 +0000

    move atomic symbols to separate file

--- generateartisatomicdatafileshilliernahar.py
+++ generateartisatomicdatafileshilliernahar.py
@@ -5,13 +5,7 @@ import sys
 import numpy as np
 import math
 
-elsymbols = ('','H','He','Li','Be','B','C','N','O','F','Ne','Na','Mg','Al','Si','P',
-             'S','Cl','Ar','K','Ca','Sc','Ti','V','Cr','Mn','Fe','Co','Ni','Cu',
-             'Zn','Ga','Ge','As','Se','Br','Kr','Rb','Sr','Y','Zr','Nb','Mo','Tc',
-             'Ru','Rh','Pd','Ag','Cd','In','Sn','Sb','Te','I','Xe','Cs','Ba','La',
-             'Ce','Pr','Nd','Pm','Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb','Lu',
-             'Hf','Ta','W','Re','Os','Ir','Pt','Au','Hg','Tl','Pb','Bi','Po','At',
-             'Rn','Fr','Ra','Ac')
+elsymbols = ['n']+[line.split()[1] for line in open('atomic_symbols.dat')]
 
 roman_numerals = ('','I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII',
                  'XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX')
