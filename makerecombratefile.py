d6bb802f1d61e39aafcd1dc3ce610cde230e42f1
Luke Shingles <luke.shingles@gmail.com>
2017-06-14 16:36:27 +0100

    Output recombination rates for Nahar or Chianti for ions in the composition file

--- makerecombratefile.py
+++ makerecombratefile.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 # import itertools
-import math
-import os
 import glob
+# import math
+import os
 import sys
-from collections import defaultdict, namedtuple
+from collections import namedtuple
 
 # import numexpr as ne
 import numpy as np
 import pandas as pd
+import pidly
+
+import artistools as at
+import makeartisatomicfiles as artisatomic
+
 # from astropy import constants as const
 # from astropy import units as u
-
-import makeartisatomicfiles as artisatomic
 
 
 def get_ionrecombrates_fromfile(filename, atomicnumber, ionstage):
@@ -55,22 +58,48 @@ def get_ionrecombrates_fromfile(filename, atomicnumber, ionstage):
 
 
 def main():
-    # T_goal = 6.31E+03
+    dir(pidly.IDL)
+    idl = pidly.IDL('/usr/local/ssw/gen/setup/ssw_idl')
+    dir(idl)
+    idl.setecho("False")
+
     atomic_number = 26
     ion_stage = 1
 
+    dfcomposition = at.get_composition_data('artis_files/compositiondata.txt')
+
     with open(os.path.join('artis_files', f'recombrates.txt'), 'w') as frecombrates:
-        rrcfiles = glob.glob('atomic-data-nahar/*rrc*.txt')
-        for filename in rrcfiles:
-            ionstr = os.path.basename(filename).split('.')[0]  # should be something like 'fe2'
-            elsymbol = ionstr.rstrip('0123456789')
-            lowerionstage = int(ionstr[len(elsymbol):])
-            upperionstage = lowerionstage + 1
-            atomic_number = artisatomic.elsymbols.index(elsymbol.title())
-            dfrecombrates = get_ionrecombrates_fromfile(filename, atomic_number, ion_stage)
-            frecombrates.write(f'{atomic_number} {upperionstage} {len(dfrecombrates)}\n')
-            for _, row in dfrecombrates.iterrows():
-                frecombrates.write(f"{row['logT']} {row['RRC_low_n']} {row['RRC_total']}\n")
+        for _, comprow in dfcomposition.iterrows():
+            atomic_number = int(comprow.Z)
+            for lowerionstage in range(int(comprow.lowermost_ionstage), int(comprow.uppermost_ionstage)):
+                upperionstage = lowerionstage + 1
+                print(f'{artisatomic.elsymbols[atomic_number]} {upperionstage}->{lowerionstage}')
+
+                print(atomic_number, lowerionstage)
+                rrcfiles = glob.glob(f'atomic-data-nahar/{artisatomic.elsymbols[atomic_number].lower()}{lowerionstage}.rrc*.txt')
+                if rrcfiles:
+                    filename = rrcfiles[0]
+                    ionstr = os.path.basename(filename).split('.')[0]  # should be something like 'fe2'
+                    elsymbol = ionstr.rstrip('0123456789')
+                    lowerionstage = int(ionstr[len(elsymbol):])
+                    upperionstage = lowerionstage + 1
+                    atomic_number = artisatomic.elsymbols.index(elsymbol.title())
+                    dfrecombrates = get_ionrecombrates_fromfile(filename, atomic_number, ion_stage)
+                    frecombrates.write(f'{atomic_number} {upperionstage} {len(dfrecombrates)}\n')
+                    for _, row in dfrecombrates.iterrows():
+                        frecombrates.write(f"{row['logT']} {row['RRC_low_n']} {row['RRC_total']}\n")
+                else:
+                    arr_logT_e = np.arange(1.0, 9.1, 0.1)
+                    frecombrates.write(f'{atomic_number} {upperionstage} {len(arr_logT_e)}\n')
+                    for logT_e in arr_logT_e:
+                        idl(f"rrc = RECOMB_RATE('{artisatomic.elsymbols[atomic_number].lower()}_{upperionstage}', {10 ** logT_e})")
+                        try:
+                            rrc = float(idl.rrc[0])
+                        except IndexError:
+                            rrc = float(idl.rrc)
+                        frecombrates.write(f"{logT_e:.1f} {-1.0} {rrc}\n")
+
+    idl.close()
 
 
 if __name__ == "__main__":
