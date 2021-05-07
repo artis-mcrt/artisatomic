4567851dbbebd3defb01e13eb643cb40ac8fb2d2
Luke Shingles <luke.shingles@gmail.com>
2021-05-07 12:11:00 +0100

    Fix transition list sorting

--- artisatomic/migrateoldatomicdata.py
+++ artisatomic/migrateoldatomicdata.py
@@ -5,8 +5,14 @@ import shutil
 from pathlib import Path
 
 
+def write_sorted_transitionblock(fileout, transitionblock):
+    transitionblock.sort()
+    for (levelid_lower, levelid_upper, A, coll_str, forbidden) in transitionblock:
+        fileout.write(f'{levelid_lower:4d} {levelid_upper:4d} {A} {coll_str:9.2e} {forbidden:d}\n')
+
+
 def main():
-    inputfolder = Path('/Volumes/GoogleDrive/Shared drives/ARTIS/artis-atomic_private/artis_files_example_old')
+    inputfolder = Path('/Volumes/GoogleDrive/Shared drives/ARTIS/artis-atomic_private/artis_classic_atomicdata')
     outputfolder = Path(__file__).parent.absolute() / 'artis_files'
 
     outputfolder.mkdir(parents=True, exist_ok=True)
@@ -23,13 +29,22 @@ def main():
     with open(inputfolder / 'LINELIST', 'r') as flinelist:
         path_transdata = outputfolder / 'transitiondata.txt'
         print(f"writing {path_transdata}")
+        transitionblock = []
+        firstblock = True
         with path_transdata.open('w') as ftransdata:
             while line := flinelist.readline():
                 row = line.split()
 
                 if len(row) == 3:  # ion header row
+                    write_sorted_transitionblock(ftransdata, transitionblock)
+                    transitionblock = []
+
                     atomic_number, ion_stage, ion_ntrans = [int(x) for x in row]
-                    ftransdata.write(f'{atomic_number:7d}{ion_stage:7d}{ion_ntrans:12d}\n')
+                    if not firstblock:
+                        ftransdata.write('\n')
+                    firstblock = False
+                    ftransdata.write(f'{atomic_number:7d}{ion_stage:7d}{ion_ntrans:12d}')
+                    transitionblock = []
 
                 elif len(row) == 4:  # transition row
                     transid = int(row[0])
@@ -47,14 +62,17 @@ def main():
                         # print(f"Flipping lower/upper {atomic_number=}, {ion_stage=}, {ion_ntrans=}: {line}")
                         levelid_lower, levelid_upper = levelid_upper, levelid_lower
 
-                    ftransdata.write(f'{levelid_lower:4d} {levelid_upper:4d} {A} {coll_str:9.2e} {forbidden:d}\n')
+                    transitionblock.append((levelid_lower, levelid_upper, A, coll_str, forbidden))
 
-                elif not line.strip():  # blank line is ok
+                elif not line.strip():  # blank line is ok
                     ftransdata.write(line)
 
                 else:
                     print(f"Unknown line content: {line}")
                     assert False
+
+            write_sorted_transitionblock(ftransdata, transitionblock)
+            transitionblock = []
 
     # prepend new lines for table size and increment
     with open(inputfolder / 'PHIXS', 'r') as fadataold:
@@ -65,7 +83,6 @@ def main():
             fadatanew.write('0.1\n')
             while line := fadataold.readline():
                 fadatanew.write(line)
-
 
 
 if __name__ == "__main__":
