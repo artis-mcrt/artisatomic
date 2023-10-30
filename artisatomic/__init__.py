c54863df1d7d87d8212b2b7019ac47556c183325
Luke Shingles <luke.shingles@gmail.com>
2023-10-30 22:46:29 +0000

    Update __init__.py

--- artisatomic/__init__.py
+++ artisatomic/__init__.py
@@ -246,7 +246,7 @@ def process_files(listelements: list[tuple[int, list[int | tuple[int, str]]]], a
                 # ]:  # atomic_number in [8, 26] and os.path.isfile(path_nahar_energy_file):  # Hillier/Nahar hybrid
                 #     handler = "hiller_nahar"
                 elif atomic_number <= 28 or atomic_number == 56:  # Hillier data only
-                    handler = "hillier"
+                    handler = "cmfgen"
                 elif atomic_number >= 57:  # DREAM database of Z > 57
                     handler = "dream"
                 else:
@@ -416,7 +416,7 @@ def process_files(listelements: list[tuple[int, list[int | tuple[int, str]]]], a
                             atomic_number, ion_stage, energy_levels[i], flog, args
                         )
 
-                elif handler == "hillier":  # Hillier data only
+                elif handler == "cmfgen":  # Hillier CMFGEN data only
                     (
                         ionization_energy_ev[i],
                         energy_levels[i],
