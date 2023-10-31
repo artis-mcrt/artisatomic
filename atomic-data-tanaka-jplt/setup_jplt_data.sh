49e1623b025ce5c718d8ba95f9000afddf776f8b
Luke Shingles <luke.shingles@gmail.com>
2023-10-31 11:13:41 +0000

    Update setup_jplt_data.sh

--- atomic-data-tanaka-jplt/setup_jplt_data.sh
+++ atomic-data-tanaka-jplt/setup_jplt_data.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-if [ ! -f data_v1.1.tar.gz ]; then curl -O http://dpc.nifs.ac.jp/DB/Opacity-Database/data/data_v1.1.tar.gz; fi
+if [ ! -f 26_1.txt.zst ]; then curl -O http://dpc.nifs.ac.jp/DB/Opacity-Database/data/data_v1.1.tar.gz; fi
 
 mkdir -p data_v1.1
 md5sum -c data_v1.1.tar.gz.md5
