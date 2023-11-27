be7acb013446468663757e721831681e67f786e3
Luke Shingles <luke.shingles@gmail.com>
2023-11-27 15:22:57 +0000

    Move entry_points to pyproject.toml

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@
 """Atomic data tools for the ARTIS 3D supernova radiative transfer code."""
 from pathlib import Path
 
-from setuptools import find_packages, setup
+from setuptools import find_packages
+from setuptools import setup
 from setuptools_scm import get_version
 
 setup(
@@ -18,13 +19,6 @@ setup(
     long_description=(Path(__file__).absolute().parent / "README.md").open("rt").read(),
     long_description_content_type="text/markdown",
     install_requires=(Path(__file__).absolute().parent / "requirements.txt").open("rt").read().splitlines(),
-    entry_points={
-        "console_scripts": [
-            "makeartisatomicfiles = artisatomic:main",
-            "makerecombratefile = artisatomic.makerecombratefile:main",
-            "convertartisatomictopythonrt = artisatomic.converttopythonrt:main",
-        ]
-    },
     python_requires=">=3.9",
     setup_requires=["psutil>=5.9.0", "setuptools>=45", "setuptools_scm[toml]>=6.2", "wheel"],
     tests_require=["pytest", "pytest-runner", "pytest-cov"],
