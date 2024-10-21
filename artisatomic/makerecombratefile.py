882eb3820cfb5665c6d0ab59f2a4a3598a6a591c
Luke Shingles <luke.shingles@gmail.com>
2024-10-21 05:22:03 +0100

    makerecombratefile: Remove IDL dependency using ChiantiPy

--- artisatomic/makerecombratefile.py
+++ artisatomic/makerecombratefile.py
@@ -5,10 +5,11 @@ import os
 import sys
 from collections import namedtuple
 
-import artistools as at
+import ChiantiPy
+import ChiantiPy.core as ch
 import numpy as np
 import pandas as pd
-import pidly
+from artistools import get_composition_data
 
 import artisatomic
 
@@ -21,7 +22,7 @@ import artisatomic
 
 def read_nahar_rrcfile(filename, noprint=False):
     if not noprint:
-        print(f"Reading {filename}")
+        print(f"  reading {filename}")
 
     header_row = []
     with open(filename) as filein:
@@ -44,8 +45,7 @@ def read_nahar_rrcfile(filename, noprint=False):
         recomb_tuple = namedtuple("recomb_tuple", ["logT", "RRC_low_n", "RRC_total"])
         records = []
         for line in filein:
-            row = line.split()
-            if row:
+            if row := line.split():
                 if len(row) != len(header_row):
                     print("Row contains wrong number of items for header:")
                     print(header_row)
@@ -53,15 +53,15 @@ def read_nahar_rrcfile(filename, noprint=False):
                     sys.exit()
                 records.append(recomb_tuple(*[float(row[index]) for index in [index_logt, index_low_n, index_tot]]))
 
-    dfrecombrates = pd.DataFrame.from_records(records, columns=recomb_tuple._fields)
-    return dfrecombrates
+    return pd.DataFrame.from_records(records, columns=recomb_tuple._fields)
 
 
 def main():
-    dir(pidly.IDL)
-    idl = pidly.IDL("/usr/local/ssw/gen/setup/ssw_idl")
-    dir(idl)
-    idl.setecho("False")
+    # dir(pidly.IDL)
+    # idl = pidly.IDL("/Applications/GNU Data Language.app/Contents/MacOS/gdl", idl_prompt="GDL> ")
+    # idl = pidly.IDL("/usr/local/ssw/gen/setup/ssw_idl")
+    # dir(idl)
+    # idl.setecho("False")
 
     # Shull & Steenberg 1982
     A_rad, X_rad = {}, {}
@@ -79,25 +79,16 @@ def main():
     A_rad[28, 5], X_rad[28, 5] = 3.85e-12, 0.746
     A_rad[28, 6], X_rad[28, 6] = 9.05e-12, 0.682
 
-    dfcomposition = at.get_composition_data("artis_files/compositiondata.txt")
+    dfcomposition = get_composition_data("artis_files/compositiondata.txt")
 
     with open(os.path.join("artis_files", "recombrates.txt"), "w") as frecombrates:
         for _, comprow in dfcomposition.iterrows():
             atomic_number = int(comprow.Z)
-            for lowerionstage in range(int(comprow.lowermost_ionstage), int(comprow.uppermost_ionstage)):
+            for lowerionstage in range(int(comprow["lowermost_ion_stage"]), int(comprow["uppermost_ion_stage"])):
                 upperionstage = lowerionstage + 1
-                print(f"{artisatomic.elsymbols[atomic_number]} {upperionstage}->{lowerionstage}")
+                print(f"Z={atomic_number} {artisatomic.elsymbols[atomic_number]} {upperionstage}->{lowerionstage}")
 
-                print(atomic_number, lowerionstage)
-                rrcfiles = glob.glob(
-                    f"atomic-data-nahar/{artisatomic.elsymbols[atomic_number].lower()}{lowerionstage}.rrc*.txt"
-                )
-
-                if False:
-                    pass
-
-                #
-                # elif atomic_number == 28:  # Pure Shull & Steenberg 1982
+                # if atomic_number == 28:  # Pure Shull & Steenberg 1982
                 #     arr_logT_e = np.arange(1.0, 9.1, 0.1)
                 #     frecombrates.write(f'{atomic_number} {upperionstage} {len(arr_logT_e)}\n')
                 #     for logT_e in arr_logT_e:
@@ -105,7 +96,9 @@ def main():
                 #         rrc = A_rad[atomic_number, lowerionstage] * (T_e / 1e4) ** - X_rad[atomic_number, lowerionstage]
                 #         frecombrates.write(f"{logT_e:.1f} {-1.0} {rrc}\n")
 
-                elif rrcfiles:  # use Nahar's values if available
+                if rrcfiles := glob.glob(
+                    f"atomic-data-nahar/{artisatomic.elsymbols[atomic_number].lower()}{lowerionstage}.rrc*.txt"
+                ):  # use Nahar's values if available
                     naharfilename = rrcfiles[0]
                     ionstr = os.path.basename(naharfilename).split(".")[0]  # should be something like 'fe2'
                     elsymbol = ionstr.rstrip("0123456789")
@@ -131,21 +124,20 @@ def main():
                 #         print(f"Log T {row['logT']} Nahar/SS1982 boost factor: {(row['RRC_total'] / rrc_fe_ss82)}")
                 #         frecombrates.write(f"{row['logT']} {-1.0} {rrc_total}\n")
 
-                else:  # otherwise use Chianti
+                else:  # use Chianti with ChiantiPy
+                    print("  using Chianti")
                     arr_logT_e = np.arange(1.0, 9.1, 0.1)
                     frecombrates.write(f"{atomic_number} {upperionstage} {len(arr_logT_e)}\n")
-                    for logT_e in arr_logT_e:
-                        idl(
-                            f"rrc = RECOMB_RATE('{artisatomic.elsymbols[atomic_number].lower()}_{upperionstage}',"
-                            f" {10 ** logT_e})"
-                        )
-                        try:
-                            rrc = float(idl.rrc[0])
-                        except IndexError:
-                            rrc = float(idl.rrc)
-                        frecombrates.write(f"{logT_e:.1f} {-1.0} {rrc}\n")
-
-    idl.close()
+                    arr_temperature = 10**arr_logT_e
+                    ion = ch.ion(
+                        f"{artisatomic.elsymbols[atomic_number].lower()}_{upperionstage}", temperature=arr_temperature
+                    )
+                    ion.rrRate()
+                    arr_rrc = ion.RrRate["rate"]
+                    ion.drRate()
+                    arr_drc = ion.DrRate["rate"]
+                    for i, logT_e in enumerate(arr_logT_e):
+                        frecombrates.write(f"{logT_e:.1f} {-1.0} {arr_rrc[i]}\n")
 
 
 if __name__ == "__main__":
