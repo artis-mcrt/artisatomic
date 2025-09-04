98f8924ff0b9caa57d678e5508e813f0dffbe103
Luke Shingles <luke.shingles@gmail.com>
2025-09-04 15:40:34 +0100

    Update setup_jplt_data.sh

--- atomic-data-tanaka-jplt/setup_jplt_data.sh
+++ atomic-data-tanaka-jplt/setup_jplt_data.sh
@@ -11,33 +11,19 @@
 # fi
 # zstd --rm -f -T0 -v data_v1.1/*.txt
 
-if [ ! -f data_v2.0/26_1.txt.zst ]; then
-
-  if [ ! -f data_v2.0.tar.gz ]; then curl -O http://dpc.nifs.ac.jp/DB/Opacity-Database/data/data_v2.0.tar.gz; fi
-
-  mkdir -p data_v2.0
-  md5sum -c data_v2.0.tar.gz.md5
-  tar -xf data_v2.0.tar.gz
-
-fi
-zstd --rm -f -T0 -v data_v2.0/*.txt
-
-# v2.1 is not complete, so should be overlaid on v2.0 files
-if [ ! -f data_v2.1/26_1.txt.zst ]; then
-
-    rm -rf data_v2.1
-    mkdir -p data_v2.1
-    rsync -av data_v2.0/ data_v2.1/
-
-fi
-
 if [ ! -f data_v2.1/33_2.txt.zst ]; then
 
+  if [ ! -f data_v2.0.tar.gz ]; then curl -O http://dpc.nifs.ac.jp/DB/Opacity-Database/data/data_v2.0.tar.gz; fi
+  md5sum -c data_v2.0.tar.gz.md5
+
   if [ ! -f grasp_v2.1.tar.gz ]; then curl -O http://dpc.nifs.ac.jp/DB/Opacity-Database/data/grasp_v2.1.tar.gz; fi
+  md5sum -c grasp_v2.1.tar.gz.md5
 
   mkdir -p data_v2.1
-  md5sum -c grasp_v2.1.tar.gz.md5
-  tar -xf grasp_v2.1.tar.gz --transform 's!^grasp_v2.1\($\|/\)!data_v2.1\1!'
+  # v2.1 is not complete, so should be overlaid on v2.0 files
+  tar -xvf data_v2.0.tar.gz -C data_v2.1 --strip-components=1
+
+  tar -xvf grasp_v2.1.tar.gz -C data_v2.1 --strip-components=1
 
 fi
 zstd --rm -f -T0 -v data_v2.1/*.txt
