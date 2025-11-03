bbdfe77571506c8c42c960fe44666c6405c4ce2f
AMG3141 <67506477+AMG3141@users.noreply.github.com>
2025-11-03 16:12:48 +0000

    Also check for the line "Oscilator strengths" (one 'l'), as this is misspelled in some of the files. (Particular case I found was in N VII).

--- artisatomic/readhillierdata.py
+++ artisatomic/readhillierdata.py
@@ -379,7 +379,7 @@ def read_levels_and_transitions(
 
         # defined_transition_ids = []
         for line in fhillierosc:
-            if line.startswith("                        Oscillator strengths"):  # only allow one table
+            if line.startswith("                        Oscillator strengths") or line.startswith("             Oscilator strengths"):  # only allow one table
                 break
             linesplitdash = line.split("-")
             row = (linesplitdash[0] + " " + "-".join(linesplitdash[1:-1]) + " " + linesplitdash[-1]).split()
