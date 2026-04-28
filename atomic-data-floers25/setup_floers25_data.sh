458d93989031b16c1819642e68c6adcee111e4b3
Luke Shingles <luke.shingles@gmail.com>
2026-04-28 16:24:53 +0100

    Update Floers+25 data to version 2

--- atomic-data-floers25/setup_floers25_data.sh
+++ atomic-data-floers25/setup_floers25_data.sh
@@ -2,20 +2,20 @@
 
 if [[ ! -f OutputFiles/57LaII_levels_calib.txt && ! -f OutputFiles/57LaII_levels_calib.txt.zst ]]; then
 
-  if [[ ! -f GSI_lanthanides_calibrated_levels.zip ]]; then curl -O https://zenodo.org/records/15835361/files/GSI_lanthanides_calibrated_levels.zip?download=1; fi
+  if [[ ! -f GSI_lanthanides_calibrated_levels.zip ]]; then curl -O https://zenodo.org/records/19335084/files/GSI_lanthanides_calibrated_levels.zip?download=1; fi
 
-  md5sum -c GSI_lanthanides_calibrated_levels.zip.md5
-  unzip -j -d OutputFiles GSI_lanthanides_calibrated_levels.zip
+  unzip -j -o -d OutputFiles GSI_lanthanides_calibrated_levels.zip
+  rm -f OutputFiles/.DS_Store OutputFiles/.*
   zstd --rm -f -T0 -15 -v OutputFiles/*.txt
 
 fi
 
 if [[ ! -f OutputFiles/57LaII_transitions_calib.txt && ! -f OutputFiles/57LaII_transitions_calib.txt.zst ]]; then
 
-  if [[ ! -f GSI_lanthanides_calibrated_transitions.zip ]]; then curl -O https://zenodo.org/records/15835361/files/GSI_lanthanides_calibrated_transitions.zip?download=1; fi
+  if [[ ! -f GSI_lanthanides_calibrated_transitions.zip ]]; then curl -O https://zenodo.org/records/19335084/files/GSI_lanthanides_calibrated_transitions.zip?download=1; fi
 
-  md5sum -c GSI_lanthanides_calibrated_transitions.zip.md5
-  unzip -j -d OutputFiles GSI_lanthanides_calibrated_transitions.zip
+  unzip -j -o -d OutputFiles GSI_lanthanides_calibrated_transitions.zip
+  rm -f OutputFiles/.DS_Store OutputFiles/.*
   zstd --rm -f -T0 -15 -v OutputFiles/*.txt
 
 fi
